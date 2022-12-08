a2dce3aaf59f653abacfe4aafb480fb9fd12715c
Kristian Nymann Jakobsen <kristian@nymann.dev>
2022-12-08 02:12:42 +0100

    :recycle: Refactor method to classes

--- src/theatrical_players_refactoring_kata_2022_dec/statement.py
+++ src/theatrical_players_refactoring_kata_2022_dec/statement.py
@@ -1,5 +1,7 @@
+from abc import ABC
+from abc import abstractmethod
 import math
-from typing import Any
+from typing import Any, Type
 
 
 def format_as_dollars(amount):
@@ -12,38 +14,61 @@ def statement(invoice: dict[str, Any], plays: dict[str, dict[str, str]]) -> str:
     result = f'Statement for {invoice["customer"]}\n'
 
     for perf in invoice["performances"]:
-        play = plays[perf["playID"]]
-        if play["type"] == "tragedy":
-            this_amount = tragedy_play_price(perf=perf)
-        elif play["type"] == "comedy":
-            this_amount = comedy_play_price(perf=perf)
-        else:
-            raise ValueError(f'unknown type: {play["type"]}')
-
-        # add volume credits
-        volume_credits += max(perf["audience"] - 30, 0)
-        if "comedy" == play["type"]:
-            volume_credits += math.floor(perf["audience"] / 5)
-        # print line for this order
-        result += f' {play["name"]}: {format_as_dollars(this_amount/100)} ({perf["audience"]} seats)\n'
-        total_amount += this_amount
+        play_dict = plays[perf["playID"]]
+        play: Play = PlayFactory.create_play(play_dict=play_dict, perf=perf)
+        volume_credits += play.volume_credits()
+        result += f' {play_dict["name"]}: {format_as_dollars(play.price()/100)} ({perf["audience"]} seats)\n'
+        total_amount += play.price()
 
     result += f"Amount owed is {format_as_dollars(total_amount/100)}\n"
     result += f"You earned {volume_credits} credits\n"
     return result
 
 
-def tragedy_play_price(perf: dict) -> float:
-    result = 40000
-    if perf["audience"] > 30:
-        result += 1000 * (perf["audience"] - 30)
-    return result
+class Play(ABC):
+    def __init__(self, perf: dict) -> None:
+        self.perf = perf
+        self.audience: int = perf["audience"]
+
+    def volume_credits(self) -> float:
+        return max(self.audience - 30, 0)
+
+    @abstractmethod
+    def price(self) -> float:
+        raise NotImplementedError
 
 
-def comedy_play_price(perf: dict) -> float:
-    this_amount = 30000
-    if perf["audience"] > 20:
-        this_amount += 10000 + 500 * (perf["audience"] - 20)
+class TragedyPlay(Play):
+    def price(self) -> float:
+        result = 40000
+        if self.audience > 30:
+            result += 1000 * (self.audience - 30)
+        return result
 
-    this_amount += 300 * perf["audience"]
-    return this_amount
+
+class ComedyPlay(Play):
+    def price(self) -> float:
+        this_amount = 30000
+        if self.audience > 20:
+            this_amount += 10000 + 500 * (self.audience - 20)
+
+        this_amount += 300 * self.audience
+        return this_amount
+
+    def volume_credits(self) -> float:
+        volume_credits = super().volume_credits()
+        volume_credits += math.floor(self.audience / 5)
+        return volume_credits
+
+
+class PlayFactory:
+    _plays = {"comedy": ComedyPlay, "tragedy": TragedyPlay}
+
+    @classmethod
+    def create_play(cls, play_dict: dict, perf: dict) -> Play:
+        play_type = play_dict["type"]
+        try:
+            play_klass: Type[Play] = cls._plays[play_type]
+        except KeyError:
+            raise ValueError(f"unknown type: {play_type}")
+        return play_klass(perf=perf)
