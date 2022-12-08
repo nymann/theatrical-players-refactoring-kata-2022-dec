c9fcb62466f599bd37dc864462c5cf5b53904b56
Kristian Nymann Jakobsen <kristian@nymann.dev>
2022-12-08 03:23:31 +0100

    :recycle: Refactor to list instead of string concat

--- src/theatrical_players_refactoring_kata_2022_dec/renderers/plaintext.py
+++ src/theatrical_players_refactoring_kata_2022_dec/renderers/plaintext.py
@@ -5,18 +5,18 @@ from theatrical_players_refactoring_kata_2022_dec.statement import Statement
 
 class PlaintextStatementRenderer(StatementRenderer):
     def render(self, statement: Statement) -> str:
-        result = f"Statement for {statement.customer}\n"
+        result: list[str] = [f"Statement for {statement.customer}"]
         for play in statement.plays:
-            result += self._format_play(play=play)
+            result.append(self._format_play(play=play))
 
-        result += self._amount_owned(statement=statement)
-        result += f"You earned {statement.volume_credits} credits\n"
-        return result
+        result.append(self._amount_owned(statement=statement))
+        result.append(f"You earned {statement.volume_credits} credits")
+        return "\n".join(result)
 
     def _format_play(self, play: Play) -> str:
         dollar_price = self.format_as_dollars(play.price() / 100)
-        return f" {play.name}: {dollar_price} ({play.audience} seats)\n"
+        return f" {play.name}: {dollar_price} ({play.audience} seats)"
 
     def _amount_owned(self, statement: Statement) -> str:
         amount_owned = self.format_as_dollars(statement.total_amount / 100)
-        return f"Amount owed is {amount_owned}\n"
+        return f"Amount owed is {amount_owned}"
